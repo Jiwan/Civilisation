7b4cf9df98ec75dd92272074c7853722625e28a7
Richard Lagrange <richardlagrange1@gmail.com>
2012-11-21 22:40:35 +0100

    Début classe Game

---
+++ Civilization/Game/Game.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Drawing;
+using Civilization.World.Map;
+using System.Runtime.Serialization;
+using System.Collections;
+
+namespace Civilization.Game
+{
+    class Game// : ISerializable
+    {
+        #region fields
+        //private IGraphicEngine engine;
+        private Window mainWindow;
+        private Civilization.World.Map.Map map;
+        private Stack states;
+        private IPlayer[] players; 
+        #endregion
+
+        #region properties
+        public IPlayer[] Players
+        {
+            get {return players;}
+        }
+        #endregion
+
+        #region constructors
+        public Game()
+        {
+            //engine = new GraphicEngine();
+            mainWindow = new Window();
+            map = new Map(new System.Drawing.Point(25, 25));
+            states = new Stack(); 
+            players = new IPlayer[2];
+        }
+
+        public Game(SerializationInfo info)
+        {
+            //engine = new GraphicEngine();
+            mainWindow = new Window();
+            // à implémenter
+        }
+        #endregion
+
+        #region methods
+        //public void AddPlayer(IPlayer newPlayer) { };
+        public void GetObjectData(SerializationInfo info){}
+        public IGameState PopState()
+        {
+            return states.Pop();
+        }
+
+        public void PushState(IGameState gameState)
+        {
+            states.Push(gameState);
+        }
+
+        #endregion
+    }
+}
---
+++ Civilization/World/Map/MediumMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace Civilization.World.Map
+{
+    class MediumMap : IMapCreate
+    {
+        public Map CreateMap(ISquareRandomizer randomizer)
+        {
+            Map map = new Map(new Point(100, 100));
+            for (int i = 0; i < 100; ++i)
+            {
+                for (int j = 0; j < 100; ++j)
+                {
+                    Point point = new Point(i, j);
+                    map.ReplaceSquare(point, randomizer.CreateSquare());
+                }
+            }
+            return map;
+        }
+    }
+}
