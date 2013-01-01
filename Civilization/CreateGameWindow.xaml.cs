9608532524810d0a503399d660e0075171579815
Jiwan <jguegant@gmail.com>
2013-01-01 21:06:43 +0100

    Ajout des resources (images)

---
+++ Civilization/CreateGameWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using Civilization.Player;
+
+namespace Civilization
+{
+    /// <summary>
+    /// Logique d'interaction pour CreateGameWindow.xaml
+    /// </summary>
+    public partial class CreateGameWindow : Window
+    {
+        #region fields
+        private List<IPlayer> players;
+        #endregion
+
+        #region constructor
+        public CreateGameWindow()
+        {
+            InitializeComponent();
+            InitializePlayers();
+        }
+        #endregion
+
+        #region methods
+        #region private
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void InitializePlayers()
+        {
+            players = new List<IPlayer>();
+            players.Add(new HumanPlayer("Joueur 1", Colors.Blue));
+            players.Add(new AIPlayer("Joueur 2", Colors.Red));
+
+            playerListBox.ItemsSource = players;
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ Civilization/TemplateSelector/PlayerTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using Civilization.Player;
+
+namespace Civilization.TemplateSelector
+{
+    class PlayerTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate HumanDataTemplate { get; set; }
+        public DataTemplate AIDataTemplate { get; set; }    
+
+        public override DataTemplate SelectTemplate(object player, DependencyObject container)
+        {
+            if (player is HumanPlayer)
+            {
+                return HumanDataTemplate;
+            }
+            else if (player is AIPlayer)
+            {
+                return AIDataTemplate;
+            }            
+            else
+            {
+                return base.SelectTemplate(player, container);
+            }
+        }
+    }
+}
